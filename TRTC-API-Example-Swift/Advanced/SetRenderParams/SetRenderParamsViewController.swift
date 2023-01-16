5316d4198585dff1315cd67e8dc299a70d239b98
adamsfliu <adamsfliu@tencent.com>
2023-01-16 10:45:00 +0800

    [iOS] update version to relesse-10.9.0.3195

--- TRTC-API-Example-Swift/Advanced/SetRenderParams/SetRenderParamsViewController.swift
+++ TRTC-API-Example-Swift/Advanced/SetRenderParams/SetRenderParamsViewController.swift
@@ -749,6 +749,7 @@ class SetRenderParamsViewController : UIViewController {
     }
     
     func showFillModeListWithIsLocal(isLocal:Bool,handler: @escaping (TRTCVideoFillMode)->Void) {
+        
         let alertTitle = isLocal ? Localize("TRTC-API-Example.RenderParams.localRenderMode") :
          Localize("TRTC-API-Example.RenderParams.remoteRenderMode")
         let alertVC = UIAlertController.init(title: alertTitle, message: nil, preferredStyle: .actionSheet)
